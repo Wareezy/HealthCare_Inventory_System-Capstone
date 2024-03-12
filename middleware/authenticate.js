463b7fb26076f02b86d8fe9fc8d7a81887249a26
Wareezy <warrenjaftha16@gmail.com>
2024-03-12 11:39:07 +0200

    token changes

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -16,10 +16,10 @@ const auth=async(req,res,next)=>{
             
             const {email}=req.body
 
-            const token=jwt.sign({email:email},process.env.SECRET_KEY,{expiresIn:'1h'})
-            res.cookie('token', token, { httpOnly: false, expiresIn:'1h'})
+            // const token=jwt.sign({email:email},process.env.SECRET_KEY,{expiresIn:'1h'})
+            // res.cookie('token', token, { httpOnly: false, expiresIn:'1h'})
             res.send({
-                token:token,
+                // token:token,
                 msg:'i have logged in!!! YAY!!!'
             })
             next()
