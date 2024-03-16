fb7eeafebfd70658da53d98242956724bff806a5
Wareezy <warrenjaftha16@gmail.com>
2024-03-16 11:17:32 +0200

    changes to auth to login

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -17,8 +17,9 @@ const auth=async(req,res,next)=>{
             console.log(currentUser)
             // const {email}=req.body
 
+            console.log(email,'This is above the token')
             const token=jwt.sign({email:email},process.env.SECRET_KEY,{expiresIn:'1h'})
-            res.cookie('token', token, { httpOnly: false, expiresIn:'1h'})
+            // res.cookie('token', token, { httpOnly: false, expiresIn:'1h'})
             res.send({
                 token:token,
                 msg:'i have logged in!!! YAY!!!',
