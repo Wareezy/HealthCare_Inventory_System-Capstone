053bb5c6ae63679aba1d4eb26b098dc792894fab
Wareezy <warrenjaftha16@gmail.com>
2024-03-16 11:25:45 +0200

    testing

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -18,7 +18,7 @@ const auth=async(req,res,next)=>{
             // const {email}=req.body
 
             console.log(email,'This is above the token')
-            const token=jwt.sign({email:email},process.env.SECRET_KEY,{expiresIn:'1h'})
+            const token=jwt.sign({currentUser:currentUser},process.env.SECRET_KEY,{expiresIn:'1h'})
             // res.cookie('token', token, { httpOnly: false, expiresIn:'1h'})
             res.send({
                 token:token,
