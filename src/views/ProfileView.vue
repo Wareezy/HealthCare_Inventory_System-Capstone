555fb9277da9862f9fd900f28b154a11d03b2deb
Wareezy <warrenjaftha16@gmail.com>
2024-03-16 15:12:25 +0200

    user profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,48 +1,43 @@
 <template>
-  <!-- doing edits on the profile page -->
-  <h1 id="avai">User</h1>
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            <div class="card my-4 mx-3" v-if="user">
-                <div class="card-body">
-                    <h5 class="card-title">{{ user.firstName }}</h5>
-                    <p class="card-text">{{ user.lastName }}</p>
-                    <p class="card-text">{{ user.userRole }}</p>
-                    <p class="card-text">{{ user.email }}</p>
-                    <p class="card-text">{{ user.password }}</p>
-                </div>
-            </div>
-            <div v-else>
-                <p>No user found</p>
-            </div>
-        </div>
-    </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      user: null
-    }
-  },
-  methods: {
-    async fetchProfile() {
-      try {
-        await this.$store.dispatch('getProfile');
-        // Once the action is completed, update the user data
-        this.user = this.$store.state.users[0];
-      } catch (error) {
-        console.error('Failed to fetch profile', error);
-      }
-    }
+  <!-- Profile display -->
+  <div>
+     <h1 id="avai">User</h1>
+     <div class="row justify-content-center">
+       <div class="col-md-4">
+         <div class="card my-4 mx-3" v-if="user">
+           <div class="card-body">
+             <h5 class="card-title">{{ user.firstName }}</h5>
+             <p class="card-text">{{ user.lastName }}</p>
+             <p class="card-text">{{ user.userRole }}</p>
+             <p class="card-text">{{ user.email }}</p>
+             <!-- Do not display the password -->
+           </div>
+         </div>
+         <div v-else>
+           <p>No user found</p>
+         </div>
+       </div>
+     </div>
+  </div>
+ </template>
+ 
+ <script>
+ export default {
+  computed: {
+     user() {
+       // Assuming the user data is stored in the first element of the users array
+       // Adjust this logic based on how your Vuex store is structured
+       return this.$store.state.users[19];
+     }
   },
   mounted() {
-    this.fetchProfile();
+     // Dispatch the getProfile action when the component is mounted
+     // Make sure to pass the correct email parameter
+     this.$store.dispatch('getProfile', '');
   }
-}
-</script>
-
-<style>
-/* Your styles */
-</style>
+ }
+ </script>
+ 
+ <style>
+ /* Your styles */
+ </style>
