7f34be39cede7cc6031f468eb93e0a17cba496b4
Wareezy <warrenjaftha16@gmail.com>
2024-03-22 15:00:58 +0200

    last minute backend changes

--- controller/inventory.js
+++ controller/inventory.js
@@ -2,36 +2,59 @@ import{getInventories,getInventory,addInventory,deleteInventory,editInventory} f
 
 // just changing the adding product routes
 export default{
-    getInvents:async(req,res)=>{
-        res.send( await getInventories())
+    getInvents: async (req, res) => {
+        try {
+            res.send(await getInventories());
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
     },
-    getInvent:async(req,res)=>{
-        res.send(await getInventory(+req.params.id))
+    
+    getInvent: async (req, res) => {
+        try {
+            res.send(await getInventory(+req.params.id));
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
     },
-    addInvents:async(req,res)=>{
-        const { productName, quantity, category, imgUrl, pricePerProduct, expenses, income} = req.body;
-        const post = await addInventory(productName, quantity, category, imgUrl, pricePerProduct, expenses, income);
-        res.send(await getInventories());  
+    
+    addInvents: async (req, res) => {
+        try {
+            const { productName, quantity, category, imgUrl, pricePerProduct, expenses, income } = req.body;
+            const post = await addInventory(productName, quantity, category, imgUrl, pricePerProduct, expenses, income);
+            res.send(await getInventories());
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
     },
-    deleteInvent:async(req,res)=>{
-        res.send(await deleteInventory(req.params.id))
+    
+    deleteInvent: async (req, res) => {
+        try {
+            res.send(await deleteInventory(req.params.id));
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
     },
-    editInvent:async(req,res)=>{
-       const [item]=await getInventory(+req.params.id)
-
-       let {productName, quantity, category, imgUrl, pricePerProduct, expenses, income}=req.body
-
-       productName ? productName=productName: {productName}=item
-       quantity ? quantity=quantity: {quantity}=item
-       category ? category=category: {category}=item
-       imgUrl ? imgUrl=imgUrl: {imgUrl}=item
-       pricePerProduct ? pricePerProduct=pricePerProduct: {pricePerProduct}=item
-       expenses ? expenses=expenses: {expenses}=item
-       income ? income=income: {income}=item
-
-       await editInventory(productName, quantity, category, imgUrl, pricePerProduct, expenses, income,+req.params.id)
-
-       res.json(await getInventories())
-
+    
+    editInvent: async (req, res) => {
+        try {
+            const [item] = await getInventory(+req.params.id);
+    
+            let { productName, quantity, category, imgUrl, pricePerProduct, expenses, income } = req.body;
+    
+            productName ? productName = productName : { productName } = item;
+            quantity ? quantity = quantity : { quantity } = item;
+            category ? category = category : { category } = item;
+            imgUrl ? imgUrl = imgUrl : { imgUrl } = item;
+            pricePerProduct ? pricePerProduct = pricePerProduct : { pricePerProduct } = item;
+            expenses ? expenses = expenses : { expenses } = item;
+            income ? income = income : { income } = item;
+    
+            await editInventory(productName, quantity, category, imgUrl, pricePerProduct, expenses, income, +req.params.id);
+    
+            res.json(await getInventories());
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
     }
 }
--- controller/users.js
+++ controller/users.js
@@ -4,53 +4,71 @@ import bcrypt from 'bcrypt'
 
  export default
  {
-
-
-getUsers:async(req,res)=>{
-    res.send( await getUsers())
-},
-
-getUser:async(req,res)=>{
-    res.send(await getUser(+req.params.id))
-},
-getProfile:async(req,res)=>{
-    const {email}=req.body;
-    res.send(await getProfile(email))
-},
-
-
-
-// addUsers:async(req,res)=>{
-//     const { firstName,lastName,userRole,email,password} = req.body;
-//     const post = await addUser(firstName,lastName,userRole,email,password);
-//     res.send(await getUsers());  
-// },
-
-//this is used to login using users credentials
-loginUser: async (req, res) => {
-    const {email,password}=req.body;
-    await checkUser(email,password)
-    // res.send(res.msg)
-},
-
-
-// this is for the signup page
-creatUser:async(req,res)=>{
-    const {firstName,lastName,userRole,email,password}=req.body
-    bcrypt.hash(password,10,async (err,hash)=>{
-        if(err) throw err
-        await addUser(firstName,lastName,userRole,email,hash)
-        res.send({
-            msg: "you have created an account"
-        })  
-     })
-},
-
-
-deleteUser:async(req,res)=>{
-    res.send(await deleteUser(req.params.id))
-},
-
+    getUsers: async (req, res) => {
+        try {
+            res.send(await getUsers());
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    },
+    
+    getUser: async (req, res) => {
+        try {
+            res.send(await getUser(+req.params.id));
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    },
+    
+    getProfile: async (req, res) => {
+        try {
+            const { email } = req.body;
+            res.send(await getProfile(email));
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    },
+    
+    // addUsers: async (req, res) => {
+    //     const { firstName, lastName, userRole, email, password } = req.body;
+    //     const post = await addUser(firstName, lastName, userRole, email, password);
+    //     res.send(await getUsers());
+    // },
+    
+    //this is used to login using users credentials
+    loginUser: async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            await checkUser(email, password);
+            // res.send(res.msg)
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    },
+    
+    // this is for the signup page
+    creatUser: async (req, res) => {
+        try {
+            const { firstName, lastName, userRole, email, password } = req.body;
+            bcrypt.hash(password, 10, async (err, hash) => {
+                if (err) throw err;
+                await addUser(firstName, lastName, userRole, email, hash);
+                res.send({
+                    msg: "you have created an account"
+                });
+            });
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    },
+    
+    deleteUser: async (req, res) => {
+        try {
+            res.send(await deleteUser(req.params.id));
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    },
 editUser:async(req,res)=>{
   
     try {
